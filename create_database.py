c1679c7b122c395d78f95949c796cc2c31b61b8d
Leandro <leandrojimenezdev@gmail.com>
2024-11-03 19:47:30 -0500

    Remove example data insertion from create_database.py for cleaner script setup and improved maintainability

--- create_database.py
+++ create_database.py
@@ -26,23 +26,4 @@ CREATE TABLE IF NOT EXISTS usuarios (
 )
 ''')
 
-
-# Insertar datos de ejemplo
-datos = [
-    ('B1', 'Lunes', '10:00', '14:00', False),
-    ('B1', 'Martes', '14:00', '18:00', True),
-    ('B2', 'Lunes', '18:00', '22:00', False),
-    ('B2', 'Martes', '10:00', '14:00', True),
-    ('B1', 'Domingo', '14:00', '18:00', False),
-
-]
-
-cursor.executemany(
-    "INSERT INTO horarios (bloque, dia, start_hour, end_hour, emergencia) VALUES (?, ?, ?, ?, ?)", datos)
-
-# Confirmar la inserción
-conn.commit()
-print("Datos de ejemplo insertados correctamente.")
-
-# Cerrar la conexión
 conn.close()
